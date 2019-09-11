f1e71c90f4a17ae78e48bca369442c2ec030c5f2
Hieu Tran <hieu.tran@viralworks.com>
2019-09-11 14:22:32 +0700

    add validator

--- Command/InitDatabaseCommand.py
+++ Command/InitDatabaseCommand.py
@@ -1,7 +1,8 @@
 import pymongo
-
+import bson
 from Command.ICommand import ICommand
 from CrawlerLib.Pymongo import MongodbClient
+import time
 
 
 class InitDatabaseCommand(ICommand):
@@ -12,8 +13,29 @@ class InitDatabaseCommand(ICommand):
     def exec(self, argv):
         db = MongodbClient.get_instance().get_crawler_db()
         #  drop database
-        db['links'].drop()
-        # init database
+        db.links.drop()
         db.create_collection('links')
+        print('drop database')
+        # init database
+        data_command = {
+                "collMod": "links",
+                "validator": {
+                    "$jsonSchema": {
+                        "bsonType": "object",
+                        "required": ["link_id", "type"],
+                        "properties": {
+                            "type": {
+                                "enum": ["FB", "IN", "YT"],
+                                "description": "can only be one of enum values [FB, IN, YT] and is required"
+                                },
+                            "link_id": {
+                                "bsonType": "string",
+                                "description": "must be a string and required"
+                                }
+                            }
+                        }
+                    }
+                }
+        db.command(data_command)
         db['links'].create_index([('link_id', pymongo.TEXT)], unique=True)
         return None
